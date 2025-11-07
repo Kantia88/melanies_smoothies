3d7aacfbabe4ee0e616b4c5816a4193957fda3ad
Kantia88 <alicelwatson@gmail.com>
2025-11-07 15:23:58 +1100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.write("The Name on Smoothie Will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect (
     'Choose up to 5 ingredients'
@@ -38,7 +38,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
-
+        st.stop()
         
 
     #st.write(ingredients_string)
